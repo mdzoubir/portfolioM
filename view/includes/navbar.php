cfadb1b77b86c9093a06a9353d9bbf34783b37bd
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-08-08 13:49:52 +0100

    first commit

---
+++ view/includes/navbar.php
@@ -0,0 +1,10 @@
+<nav class="d-flex justify-content-between menu w-75">
+    <img src="assets/images/logo.jpg" alt="" class="logo">
+    <ul class="d-flex align-self-center mt-3">
+        <li class="mr-5 des" ><a href="index.php">HOME</a> </li>
+        <li class="mr-5"><a href="portfolio.php">PORTFOLIO</a> </li>
+        <li class="mr-5"><a href="propos.php">A PROPOES</a></li>
+        <li class="mr-5"><a href="contact.php">CONTACT</a> </li>
+        <li class="mr-5"><a href="login.php">LOGIN</a> </li>
+    </ul>
+</nav>
